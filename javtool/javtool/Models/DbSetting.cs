a1b64fec1b9d63692841f66ba386dae37453345a
Cuong Bui <cuongvnm.dh@gmail.com>
2017-06-06 13:21:23 +0700

    init

---
+++ javtool/javtool/Models/DbSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace javtool.Models
+{
+    public class DbSetting
+    {
+        public string MySqlConnectionString { get; set; }
+    }
+}
